4972cb86b6a8b945f9f84e9b1ec455b8c7aed722
Daniel Bonasser <danielbonasser@gmail.com>
2023-01-25 12:12:48 -0300

    LPS-173684 Create base project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// Imports the @clayui/css package CSS
+import "@clayui/css/lib/css/atlas.css";
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app-test">
+      <h1 className="h1-test-sass">New App</h1>
+    </div>
+  )
+}
+
+export default App
