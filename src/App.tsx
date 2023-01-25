9e2d35780981fbb758fe19b5b116b2e8b772b3fc
Daniel Bonasser <danielbonasser@gmail.com>
2023-01-25 13:18:16 -0300

    LPS-173705 Create Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 // Imports the @clayui/css package CSS
 import "@clayui/css/lib/css/atlas.css";
 
-import './App.scss';
+import {Header} from "./components/Header/Header";
 
 function App() {
   return (
     <div className="app-test">
-      <h1 className="h1-test-sass">New App</h1>
+      <Header 
+        title="Test"
+        description="Description Test"
+      />
     </div>
   )
 }
