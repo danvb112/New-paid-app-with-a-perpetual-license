1a6feed08ac439d8023115dcd46b0fae71db064c
Daniel Bonasser <danielbonasser@gmail.com>
2023-01-26 11:08:10 -0300

    LPS-173771 Create Section component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 // Imports the @clayui/css package CSS
 import "@clayui/css/lib/css/atlas.css";
 
+import './App.scss'
+import { Section } from "./components/Section/Section";
+import { Input } from "./components/Input/Input";
+
 function App() {
   return (
     <div className="app-test">
-      <h1 className="h1-test-sass">New App</h1>
+      <Section>
+        <Input component="input" label="Test" required tooltip="Text"/>
+      </Section>
     </div>
   )
 }
