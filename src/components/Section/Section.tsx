73583fd4093499942475753ec8b143dd7d90b496
Daniel Bonasser <danielbonasser@gmail.com>
2023-01-27 10:30:04 -0300

    LPS-173855 Extract Tooltip to its own component

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -4,15 +4,23 @@ import {FieldBase} from '../FieldBase';
 import './Section.scss';
 
 interface SectionProps {
+    label?: string;
+    tooltip?: string;
+    tooltipText?: string;
     children: ReactNode;
 }
 
-export function Section({children}: SectionProps) {
+export function Section({
+    label,
+    tooltip,
+    tooltipText,
+    children
+}: SectionProps) {
     return(
         <FieldBase
-            label='App Info'
-            tooltip='More Info'
-            tooltipText='More Info'
+            label={label}
+            tooltip={tooltip}
+            tooltipText={tooltipText}
         >
             <div className='section-divider'></div>
 
