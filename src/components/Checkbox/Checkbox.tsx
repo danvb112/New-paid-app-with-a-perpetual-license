15311466d8e6de0e585724f29371d3559a68ceb8
Daniel Bonasser <danielbonasser@gmail.com>
2023-01-27 09:51:45 -0300

    LPS-173855 Create custom Checkbox component

---
+++ src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,36 @@
+import {ClayCheckbox} from '@clayui/form';
+
+import './Checkbox.scss'
+
+interface CheckboxProps {
+    label?: string;
+    description?: string;
+    checked: boolean;
+    onChange: () => void;
+    readOnly?: boolean;
+}
+
+export function Checkbox({
+    checked,
+    onChange,
+    description,
+    label,
+    readOnly = false
+}: CheckboxProps) {
+    return (
+        <div className='checkbox-base-container'>
+            <ClayCheckbox
+                readOnly={readOnly}
+                checked={checked}
+                onChange={() => onChange()}
+            />
+            
+            <div className='checkbox-texts-container'>
+                <span className='checkbox-label-text'>{label}</span>
+                <span className='checkbox-description-text'>
+                    {description}
+                </span>
+            </div>
+        </div>
+    );
+}
