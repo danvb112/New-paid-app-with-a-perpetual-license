7d664e32666c2b43c8078ead4c34be9ea6fdfed3
Daniel Bonasser <danielbonasser@gmail.com>
2023-01-30 16:52:23 -0300

    LPS-174016 Create DashboardNavigation component

---
+++ src/components/DashboardNavigation/DashboardNavigationList.tsx
@@ -0,0 +1,73 @@
+import classNames from 'classnames';
+
+import './DashboardNavigationList.scss'
+import { DashboardNavigationListItem } from './DashboardNavigationListItem';
+import {DashboardListItems} from './DashboardNavigation';
+
+
+interface DashboardNavigationListProps {
+    navigationItemsMock: DashboardListItems[];
+    navigationItemMock: DashboardListItems;
+    onItemsChange: (values: DashboardListItems[]) => void;
+}
+
+export function DashboardNavigationList({
+    navigationItemsMock,
+    navigationItemMock,
+    onItemsChange,
+}: DashboardNavigationListProps) {
+    const {itemIcon, itemTitle, itemName, itemSelected, items} = navigationItemMock;
+
+    return (
+        <>
+            <div 
+                className={classNames('dashboard-navigation-body-list', {
+                'dashboard-navigation-body-list-selected': itemSelected
+                })}
+                onClick={() => {
+                    const newItems = navigationItemsMock.map(navigationItem => {
+                        if(navigationItem.itemName === itemName) {
+                            return {
+                                ...navigationItem,
+                                itemSelected: true
+                            }
+                        }
+
+                        return {
+                            ...navigationItem,
+                            itemSelected: false
+                        }
+
+                    });
+                    
+                    onItemsChange(newItems);
+                }}
+            >
+                <img
+                    className={classNames('dashboard-navigation-body-list-icon', {
+                        'dashboard-navigation-body-list-icon-selected': itemSelected
+                    })}
+                    src={itemIcon} 
+                    alt="Apps icon" 
+                />
+                <span 
+                    className={classNames('dashboard-navigation-body-list-text', {
+                        'dashboard-navigation-body-list-text-selected': itemSelected
+                    })}
+                >
+                    {itemTitle}
+                </span>
+            </div>
+            
+            {itemSelected && items?.map(item => (
+                <DashboardNavigationListItem 
+                    key={item.title}
+                    icon={item.icon}
+                    title={item.title}
+                    version={item.version}
+                    status={item.status}
+                />
+            ))}
+        </>
+    );
+}
