ef01647087bc3c9670d8cd0e6f6a7654b3936630
Daniel Bonasser <danielbonasser@gmail.com>
2023-02-02 20:47:23 -0300

    LPS-174573 Create Dashboard Page

--- src/components/DashboardNavigation/DashboardNavigationListItem.tsx
+++ src/components/DashboardNavigation/DashboardNavigationListItem.tsx
@@ -5,38 +5,38 @@ import circleFill from '../../assets/icons/circle_fill.svg';
 import './DashboardNavigationListItem.scss';
 
 interface DashboardNavigationListItem {
-    icon: string;
-    title: string;
+    image: string;
+    name: string;
     version: string;
     status: string;
 }
 
 export function DashboardNavigationListItem({
-    icon,
-    title,
+    image,
+    name,
     version,
     status,
 }: DashboardNavigationListItem) {
     return (
         <div className='dashboard-navigation-body-list-item'>
-        <div>
+            <div>
+                <img
+                    className='dashboard-navigation-body-list-item-app-logo'
+                    src={image}
+                    alt="App Image"
+                />
+                <span className='dashboard-navigation-body-list-item-app-title'>{name}</span>
+                <span className='dashboard-navigation-body-list-item-app-version'>{version}</span>
+            </div>
             <img
-                className='dashboard-navigation-body-list-item-app-logo' 
-                src={icon} 
-                alt="App Icon" 
+                className={classNames('dashboard-navigation-body-list-item-app-status', {
+                    'dashboard-navigation-body-list-item-app-status-hidden': status === 'Hidden',
+                    'dashboard-navigation-body-list-item-app-status-pending': status === 'Pending',
+                    'dashboard-navigation-body-list-item-app-status-published': status === 'Published',
+                })}
+                src={circleFill}
+                alt="Circle fill"
             />
-            <span className='dashboard-navigation-body-list-item-app-title'>{title}</span>
-            <span className='dashboard-navigation-body-list-item-app-version'>{version}</span>
         </div>
-        <img
-            className={classNames('dashboard-navigation-body-list-item-app-status', {
-                'dashboard-navigation-body-list-item-app-status-hidden': status === 'hidden',
-                'dashboard-navigation-body-list-item-app-status-pending': status === 'pending',
-                'dashboard-navigation-body-list-item-app-status-published': status === 'published',
-            })} 
-            src={circleFill} 
-            alt="Circle fill" 
-        />
-    </div>
     );
 }
---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,121 @@
+import { DashboardListItems, DashboardNavigation } from "../components/DashboardNavigation/DashboardNavigation";
+import { DashboardToolbar } from "../components/DashboardToolbar/DashboardToolbar";
+
+import accountLogo from '../assets/icons/mainAppLogo.svg';
+import salesIcon from '../assets/icons/sales-icon.svg';
+import membersIcon from '../assets/icons/person-fill.svg';
+import accountIcon from '../assets/icons/account-icon.svg';
+import appsIcon from '../assets/icons/apps-fill.svg';
+import appIconTransport from '../assets/icons/app-icon-transport.svg';
+import appIconSales from '../assets/icons/app-icon-sales.svg';
+import appIconPayments from '../assets/icons/app-icon-payments.svg';
+import { AppProps, DashboardTable } from "../components/DashboardTable/DashboardTable";
+import { Header } from "../components/Header/Header";
+
+import './DashboardPage.scss';
+import { Footer } from "../components/Footer/Footer";
+
+const appList: AppProps[] = [
+    {
+        image: appIconTransport,
+        name: "A&Co Transport",
+        rating: '4.3',
+        status: 'Published',
+        type: "SaaS",
+        updatedBy: 'by Hanna White',
+        updatedDate: "Feb 14, 2023",
+        updatedResponsible: 'you',
+        version: "1.40"
+    },
+    {
+        image: appIconSales,
+        name: "A&Co Sales",
+        rating: '4.7',
+        status: 'Pending',
+        type: "OSGI",
+        updatedBy: 'by Hanna White',
+        updatedDate: "Feb 14, 2023",
+        updatedResponsible: 'you',
+        version: "2.28"
+    },
+    {
+        image: appIconPayments,
+        name: "A&Co Payments",
+        rating: '4.1',
+        status: 'Hidden',
+        type: "OSGI",
+        updatedBy: 'by Hanna White',
+        updatedDate: "Feb 14, 2023",
+        updatedResponsible: 'you',
+        version: "1.0"
+    },
+];
+
+const dashboardNavigationItems: DashboardListItems[] = [
+    {
+        itemIcon: appsIcon,
+        itemTitle: "Apps",
+        itemName: 'apps',
+        items: appList,
+        itemSelected: false
+    },
+    {
+        itemIcon: salesIcon,
+        itemTitle: "Sales",
+        itemName: 'sales',
+        itemSelected: false
+    },
+    {
+        itemIcon: membersIcon,
+        itemTitle: "Members",
+        itemName: 'members',
+        itemSelected: false
+    },
+    {
+        itemIcon: accountIcon,
+        itemTitle: "Account",
+        itemName: 'account',
+        itemSelected: false
+    },
+]
+
+
+export function DashboardPage() {
+    return (
+        <div className="dashboard-page-container">
+            <div>
+                <div className="dashboard-page-header-container">
+                    <DashboardToolbar />
+                </div>
+                <div className="dashboard-page-body-container">
+                    <DashboardNavigation
+                        accountAppsNumber="4"
+                        accountIcon={accountLogo}
+                        accountTitle="Acme Co"
+                        dashboardNavigationItems={dashboardNavigationItems}
+                    />
+
+                    <div>
+                        <div className="dashboard-page-body-header-container">
+                            <Header
+                                title="Apps"
+                                description="Manage and publish apps on the Marketplace"
+                            />
+
+                            <button className="dashboard-page-body-header-button">
+                                + New App
+                            </button>
+                        </div>
+
+                        <DashboardTable
+                            apps={appList}
+                        />
+
+                    </div>
+                </div>
+            </div>
+
+            <Footer />
+        </div>
+    );
+}
