d595488c71d78ff521de54ee438acd5baa105be0
Aline Cantarelli <aline.cantarelli@liferay.com>
2023-02-06 19:06:59 -0300

    LPS-173684 Create ProvideVersionDetailsPage page

--- src/components/FieldBase.tsx
+++ src/components/FieldBase.tsx
@@ -11,30 +11,25 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+import ClayButton from "@clayui/button";
+import ClayForm from "@clayui/form";
+import classNames from "classnames";
+import { ReactNode } from "react";
 
-import ClayForm from '@clayui/form';
-import { ClayTooltipProvider } from '@clayui/tooltip';
-import classNames from 'classnames';
-import { ReactNode } from 'react';
+import arrowDown from "../assets/icons/arrow-down.svg";
+import asteriskIcon from "../assets/icons/asterisk.svg";
 
-import asteriskIcon from '../assets/icons/asterisk.svg';
-
-import './FieldBase.scss';
-import { Tooltip } from './Tooltip/Tooltip';
+import "./FieldBase.scss";
+import { Tooltip } from "./Tooltip/Tooltip";
 
 function RequiredMask() {
 	return (
 		<>
 			<span className="field-base-required-asterisk">
-				<img
-					className="field-base-required-asterisk-icon"
-					src={asteriskIcon}
-				/>
+				<img className="field-base-required-asterisk-icon" src={asteriskIcon} />
 			</span>
 
-			<span className="hide-accessible sr-only">
-				Mandatory
-			</span>
+			<span className="hide-accessible sr-only">Mandatory</span>
 		</>
 	);
 }
@@ -45,8 +40,11 @@ interface FieldBaseProps {
 	description?: string;
 	disabled?: boolean;
 	errorMessage?: string;
+	helpMessage?: string;
+	hideFeedback?: boolean;
 	id?: string;
 	label?: string;
+	localized?: boolean;
 	required?: boolean;
 	tooltip?: string;
 	tooltipText?: string;
@@ -60,7 +58,10 @@ export function FieldBase({
 	disabled,
 	errorMessage,
 	id,
+	helpMessage,
+	hideFeedback,
 	label,
+	localized,
 	required,
 	tooltip,
 	tooltipText,
@@ -69,35 +70,52 @@ export function FieldBase({
 	return (
 		<ClayForm.Group
 			className={classNames(className, {
-				'has-error': errorMessage,
-				'has-warning': warningMessage && !errorMessage,
+				"has-error": errorMessage,
+				"has-warning": warningMessage && !errorMessage,
 			})}
 		>
-			<div className='field-base-container'>
-				{label && (
-					<label className={classNames({ disabled })} htmlFor={id}>
-						{label}
+			<div className="field-base-container">
+				<div className="field-base-container_label">
+					{label && (
+						<label className={classNames({ disabled })} htmlFor={id}>
+							{label}
 
-						{required && <RequiredMask />}
-					</label>
-				)}
+							{required && <RequiredMask />}
+						</label>
+					)}
 
-				{tooltip && (
-					<div className='field-base-tooltip'>
-						<Tooltip
-							tooltip={tooltip}
-							tooltipText={tooltipText}
-						/>
+					{tooltip && (
+						<div className="field-base-tooltip">
+							<Tooltip tooltip={tooltip} tooltipText={tooltipText} />
+						</div>
+					)}
+				</div>
+
+				{localized && (
+					<div className="field-base-localized-field">
+						<ClayButton displayType={null}>
+							{"English (US)"}
+							<img className="arrow-down-icon" src={arrowDown} />
+						</ClayButton>
+
+						<>
+							&nbsp;
+							<Tooltip
+								tooltip={"choose a language"}
+								tooltipText={tooltipText}
+							/>
+						</>
 					</div>
 				)}
 			</div>
 
 			{description && (
-				<span className='field-base-description'>{description}</span>
+				<span className="field-base-description">{description}</span>
 			)}
 
-
 			{children}
+			{!hideFeedback &&
+				helpMessage && <div className='field-base-feedback'>{helpMessage}</div>}
 		</ClayForm.Group>
 	);
 }
