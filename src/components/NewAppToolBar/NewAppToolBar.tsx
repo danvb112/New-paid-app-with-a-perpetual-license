aa3897beaf2f90c9613ae2cd670f7abf955a4b29
Aline Cantarelli <aline.cantarelli@liferay.com>
2023-02-08 18:22:01 -0300

    LPS-174698 Create dropdown

--- src/components/NewAppToolBar/NewAppToolBar.tsx
+++ src/components/NewAppToolBar/NewAppToolBar.tsx
@@ -1,19 +1,24 @@
 import ClayManagementToolbar from '@clayui/management-toolbar';
 import ClayButton from '@clayui/button';
+import { ClayDropDownWithItems } from '@clayui/drop-down';
+
 import { useNavigate } from 'react-router-dom';
 
 import mainAccountLogo from '../../assets/icons/mainAppLogo.svg';
 import emptyImage from '../../assets/icons/emptyImage.svg';
 import chevronRight from '../../assets/icons/chevron-right.svg';
 import circleFill from '../../assets/icons/circle_fill.svg';
+import dotsIcon from '../../assets/icons/dots-icon.svg';
 
 import './NewAppToolBar.scss';
+import ClayIcon from '@clayui/icon';
 
 interface NewAppToolBarProps {
-    accountName: string
-    appName?: string
-    appImage?: string
+    accountName: string;
+    appName?: string;
+    appImage?: string;
     accountImage?: string;
+    enableDropdown?: boolean;
 }
 
 export function NewAppToolBar({
@@ -21,8 +26,42 @@ export function NewAppToolBar({
     appName,
     appImage,
     accountImage,
+    enableDropdown
 }: NewAppToolBarProps) {
     const navigate = useNavigate();
+
+    type Item = {
+        disabled?: boolean;
+        label?: string;
+        type?:
+        | 'checkbox'
+        | 'contextual'
+        | 'group'
+        | 'item'
+        | 'radio'
+        | 'radiogroup'
+        | 'divider';
+    };
+
+    const items: Item[] = [
+        {
+            disabled: true,
+            label: 'Publish app',
+        },
+        {
+            disabled: true,
+            label: 'Hide app',
+        },
+        {
+            label: 'Menu List Text',
+        },
+        {
+            type: 'divider',
+        },
+        {
+            label: 'Remove app',
+        },
+    ];
 
     return (
         <div className='new-app-tool-bar-container'>
@@ -34,9 +73,7 @@ export function NewAppToolBar({
                         alt='Main account logo'
                     />
 
-                    <span
-                        className='new-app-tool-bar-main-account-logo-text'
-                    >
+                    <span className='new-app-tool-bar-main-account-logo-text'>
                         {accountName}
                     </span>
                 </div>
@@ -44,23 +81,20 @@ export function NewAppToolBar({
                 <img
                     className='new-app-tool-bar-arrow-right'
                     src={chevronRight}
-                    alt="Arrow right"
+                    alt='Arrow right'
                 />
 
                 <div className='new-app-tool-bar-new-app-logo'>
                     <img
                         className='new-app-tool-bar-new-app-logo-img'
                         src={appImage ?? emptyImage}
-                        alt="New App logo"
+                        alt='New App logo'
                     />
 
-                    <span
-                        className='new-app-tool-bar-new-app-logo-text'
-                    >
+                    <span className='new-app-tool-bar-new-app-logo-text'>
                         {appName ?? 'New App'}
                     </span>
                 </div>
-
             </ClayManagementToolbar.ItemList>
 
             <ClayManagementToolbar.ItemList expand>
@@ -68,13 +102,9 @@ export function NewAppToolBar({
                     <img
                         className='new-app-tool-bar-status-icon'
                         src={circleFill}
-                        alt="Status"
+                        alt='Status'
                     />
-                    <span
-                        className='new-app-tool-bar-status-text'
-                    >
-                        Draft
-                    </span>
+                    <span className='new-app-tool-bar-status-text'>Draft</span>
                 </div>
             </ClayManagementToolbar.ItemList>
 
@@ -85,25 +115,31 @@ export function NewAppToolBar({
                         displayType={null}
                         onClick={() => navigate('/')}
                     >
-                        <span
-                            className='new-app-tool-bar-button-text'
-                        >
-                            Exit
-                        </span>
+                        <span className='new-app-tool-bar-button-text'>Exit</span>
                     </ClayButton>
 
-                    <button
-                        className='new-app-tool-bar-button-save-draft'
-                    >
+                    <button className='new-app-tool-bar-button-save-draft'>
                         Save as draft
                     </button>
 
-                    <button
-                        className='new-app-tool-bar-button-preview-storefront'
-                    >
+                    <button className='new-app-tool-bar-button-preview-storefront'>
                         Preview Storefront
                     </button>
                 </ClayButton.Group>
+                {enableDropdown && <div className='new-app-tool-bar-button-dropdown'>
+                    <ClayDropDownWithItems
+                        items={items}
+                        trigger={
+                            <ClayButton displayType={null}>
+                                <img
+                                    className='new-app-tool-bar-button-dropdown-icon'
+                                    src={dotsIcon}
+                                    alt='Icon'
+                                />
+                            </ClayButton>
+                        }
+                    />
+                </div>}
             </ClayManagementToolbar.ItemList>
         </div>
     );
