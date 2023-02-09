2c4ddc7beb9696d6c288b4ea069eca7f1036164b
Daniel Bonasser <danielbonasser@gmail.com>
2023-02-09 09:59:53 -0300

    Remote setCurrentFlow when the page is ReviewAndSubmit

--- src/pages/AppCreationFlow/AppCreationFlow.tsx
+++ src/pages/AppCreationFlow/AppCreationFlow.tsx
@@ -262,7 +262,6 @@ export function AppCreationFlow() {
                                 selectedItem: ""
                             });
 
-                            setCurrentFlow('');
                         }}
                     />
                 )}
