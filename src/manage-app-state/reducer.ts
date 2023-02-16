121e938fac08fef2fd39b02079644dc47553280e
Igor Franca <igor.franca@liferay.com>
2023-02-16 15:30:31 -0300

    LPS-175535 Remove submit support from action type and reduce

--- src/manage-app-state/reducer.ts
+++ src/manage-app-state/reducer.ts
@@ -1,14 +1,5 @@
 import { InitialStateProps } from "./AppManageState";
 import { TYPES } from "./actionTypes";
-import {
-  createSubscription,
-  createSpecification,
-  createProductSpecification,
-  patchAppByExternalReferenceCode,
-  submitAttachment,
-  submitImage,
-} from "../utils/api";
-import { update } from "lodash";
 
 export type TAction = {
   payload?: any;
@@ -33,9 +24,6 @@ export function appReducer(state: InitialStateProps, action: TAction) {
       };
     }
     case TYPES.SUBMIT_APP_STOREFRONT: {
-      return state;
-    }
-    case TYPES.SUBMIT_APP_SUPPORT: {
       return state;
     }
     case TYPES.UPDATE_APP_BUILD: {
