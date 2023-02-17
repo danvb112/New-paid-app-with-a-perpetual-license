5c730282fdb3cd655bb3628960ccaea01374ac87
Nara Andrade <nara.andrade@liferay.com>
2023-02-17 17:50:05 -0300

    LPS-175534 Display app info on Review and Submit page

--- src/pages/ReviewAndSubmitAppPage/ReviewAndSubmitAppPageUtil.ts
+++ src/pages/ReviewAndSubmitAppPage/ReviewAndSubmitAppPageUtil.ts
@@ -30,17 +30,7 @@ export type ReviewAndSubmitAppPageUtilProps = {
 };
 
 import brightnessEmptyIcon from "../../assets/icons/brightness-empty.svg";
-import documentationIcon from "../../assets/icons/documentation-icon.svg";
-import phoneIcon from "../../assets/icons/phone-icon.svg";
-import identifiersIcon from "../../assets/icons/identifiers-icon.svg";
-import browsingHistoryIcon from "../../assets/icons/browsing-history-icon.svg";
-import financialIcon from "../../assets/icons/financial-icon.svg";
-import contactIcon from "../../assets/icons/person-fill.svg";
-import guideIcon from "../../assets/icons/guide-icon.svg";
-import userDataIcon from "../../assets/icons/user-data-icon.svg";
-import globeIcon from "../../assets/icons/globe-icon.svg";
-import diagnosticsIcon from "../../assets/icons/diagnostics-icon.svg";
-import usageTermsIcon from "../../assets/icons/usage-terms-icon.svg";
+
 import scheduleIcon from "../../assets/icons/schedule-icon.svg";
 import file1 from "../../assets/images/file1.svg";
 import file2 from "../../assets/images/file2.svg";
@@ -66,7 +56,6 @@ export const initialReviewAndSubmitAppPageItems: ReviewAndSubmitAppPageUtilProps
       fileName: "a-co-libraries-01.lpkg",
     },
     {
-      description: "The app is offered in the Marketplace with no charge.",
       icon: brightnessEmptyIcon,
       section: "Pricing",
       title: "Free",
@@ -79,20 +68,6 @@ export const initialReviewAndSubmitAppPageItems: ReviewAndSubmitAppPageUtilProps
     },
     {
       section: "Storefront",
-      files: [
-        {
-          fileDescription:
-            "Morbi leo risus, porta ac consectetur ac, vestibulum at eros.",
-          fileName: "file-name",
-          image: file1,
-        },
-        {
-          fileDescription:
-            "Morbi leo risus, porta ac consectetur ac, vestibulum at eros.",
-          fileName: "Image 2",
-          image: file2,
-        },
-      ],
     },
     {
       description:
@@ -102,33 +77,6 @@ export const initialReviewAndSubmitAppPageItems: ReviewAndSubmitAppPageUtilProps
       version: "0.0.1",
     },
     {
-      cardInfos: [
-        {
-          icon: phoneIcon,
-          title: "Support URL",
-          link: "acme.co/support",
-        },
-        {
-          icon: globeIcon,
-          title: "Publisher website URL",
-          link: "acme.co",
-        },
-        {
-          icon: usageTermsIcon,
-          title: "App usage terms (EULA) URL",
-          link: "acme.co/eula",
-        },
-        {
-          icon: documentationIcon,
-          title: "App documentation URL",
-          link: "acme.co/docs",
-        },
-        {
-          icon: guideIcon,
-          title: "App installation guide URL",
-          link: "acme.co/apps/data-viz",
-        },
-      ],
       section: "Support & Help",
     },
   ];
